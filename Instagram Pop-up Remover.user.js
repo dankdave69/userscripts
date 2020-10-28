ec501e01d189b8c6eddcdc46852662b1cc7863e3
pete the cat <71425450+dankdave69@users.noreply.github.com>
2020-10-28 08:52:39 -0700

    Update Instagram Pop-up Remover.user.js

--- Instagram Pop-up Remover.user.js
+++ Instagram Pop-up Remover.user.js
@@ -1,36 +1,58 @@
 // ==UserScript==
-// @name         Instagram Pop-up Remover
+// @name         Browse Instagram (Removes login/register Pop-up and opens up links)
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Removes annoying login/register popup when scrolling down a profile
+// @version      0.6
+// @description  Removes annoying login/register popup when scrolling down a profile; Adds [ X ] button on the popup. WIP
 // @author       dankdave69
 // @match        https://www.instagram.com/*
 // @grant        none
 // ==/UserScript==
-
 (function() {
     'use strict';
     var body = document.getElementsByTagName("body")[0];
     var popUp = document.getElementsByClassName("RnEpo  _Yhr4");
     var button = document.createElement('button');
 
-    function checkExist(){
-        if (popUp.length > 0) {
-            document.getElementsByClassName("RnEpo  _Yhr4")[0].appendChild(button);
-        };
-    };
+    function appendButton() {
+        var buttonX;
+        var buttonY;
+        var winX = document.documentElement.clientWidth;
+        var winY = document.documentElement.clientHeight;
+        winX = document.documentElement.clientWidth;
+        winY = document.documentElement.clientHeight;
+        buttonX = (winX / 2) + 200 - 45;
+        buttonY = (winY / 2) - 235.5 + 18.5;
 
-    setInterval(checkExist, 1000);
+        button.style = "position:fixed; top:" + buttonY + "px; left:" + buttonX + "px; background-color: white; border: none; color: #8e8e8e; padding: 0px 0px; text-align: center; text-decoration: none; font-family: ; display: inline-block; font-size: 20px; margin: 0px 0px; cursor: pointer; width: 30px; height: 30px";
+        button.innerHTML = "&#10006;"
+        button.onclick = function() {
+            remove();
+        }
 
-    button.style = "position:fixed; top:0px; left:0px; background-color: white; border: none; color: black; padding: 15px 30px; text-align: center; text-decoration: none; font-family: Tahoma, Geneva, sans-serif; display: inline-block; font-size: 24px; margin: 4px 2px; cursor: pointer;";
-    button.innerHTML = "Click to remove!";
-    button.onclick = function() {
-        remove();
+        document.getElementsByClassName("RnEpo  _Yhr4")[0].appendChild(button);
     }
 
-	function remove(){
+    function remove() {
         body.style.overflow = "visible";
         document.getElementsByClassName("RnEpo  _Yhr4")[0].remove();
     };
+
+    function openLinks() {
+        var list = document.querySelectorAll("a");
+        for (var i = 0; i < list.length; i++) {
+            list.item(i).onclick = function() {
+                location.href = this.href;
+            };
+        };
+    };
+
+    function checkExist() {
+        if (popUp.length > 0) {
+            appendButton();
+        };
+    };
+
+    setInterval(checkExist, 500);
+    setInterval(openLinks, 0);
 
 })();
