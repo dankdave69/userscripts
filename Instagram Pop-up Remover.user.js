9016042d3104ffbcdbd87741fb2b5c55ed30d204
pete the cat <71425450+dankdave69@users.noreply.github.com>
2020-10-28 23:59:13 -0700

    Update Instagram Pop-up Remover.user.js

--- Instagram Pop-up Remover.user.js
+++ Instagram Pop-up Remover.user.js
@@ -1,27 +1,38 @@
 // ==UserScript==
-// @name         Browse Instagram (Removes login/register Pop-up and opens up links)
-// @namespace    http://tampermonkey.net/
-// @version      0.6
-// @description  Removes annoying login/register popup when scrolling down a profile; Adds [ X ] button on the popup. WIP
+// @name         Browse Instagram Without Logging In (Removes login/register Pop-up and opens up links)
+// @namespace    https://github.com/dankdave69
+// @version      0.8
+// @description  Removes annoying login/register popup when scrolling down a profile by adding an [ X ] button on the popup and allows you to see posts without logging on.
 // @author       dankdave69
 // @match        https://www.instagram.com/*
 // @grant        none
 // ==/UserScript==
+
 (function() {
     'use strict';
     var body = document.getElementsByTagName("body")[0];
     var popUp = document.getElementsByClassName("RnEpo  _Yhr4");
     var button = document.createElement('button');
 
+    setInterval(function openLinks() {
+        var list = document.querySelectorAll("a");
+        for (var i = 0; i < list.length; i++) {
+            list.item(i).onclick = function() {
+                location.href = this.href;
+            };
+        };
+    }, 0);
+
+    function remove() {
+        body.style.overflow = "visible";
+        document.getElementsByClassName("RnEpo  _Yhr4")[0].remove();
+    };
+
     function appendButton() {
-        var buttonX;
-        var buttonY;
         var winX = document.documentElement.clientWidth;
         var winY = document.documentElement.clientHeight;
-        winX = document.documentElement.clientWidth;
-        winY = document.documentElement.clientHeight;
-        buttonX = (winX / 2) + 200 - 45;
-        buttonY = (winY / 2) - 235.5 + 18.5;
+        var buttonX = (winX / 2) + 200 - 45;
+        var buttonY = (winY / 2) - 235.5 + 18.5;
 
         button.style = "position:fixed; top:" + buttonY + "px; left:" + buttonX + "px; background-color: white; border: none; color: #8e8e8e; padding: 0px 0px; text-align: center; text-decoration: none; font-family: ; display: inline-block; font-size: 20px; margin: 0px 0px; cursor: pointer; width: 30px; height: 30px";
         button.innerHTML = "&#10006;"
@@ -32,27 +43,13 @@
         document.getElementsByClassName("RnEpo  _Yhr4")[0].appendChild(button);
     }
 
-    function remove() {
-        body.style.overflow = "visible";
-        document.getElementsByClassName("RnEpo  _Yhr4")[0].remove();
-    };
-
-    function openLinks() {
-        var list = document.querySelectorAll("a");
-        for (var i = 0; i < list.length; i++) {
-            list.item(i).onclick = function() {
-                location.href = this.href;
-            };
-        };
-    };
-
     function checkExist() {
         if (popUp.length > 0) {
             appendButton();
+            window.addEventListener('resize', appendButton);
         };
     };
 
     setInterval(checkExist, 500);
-    setInterval(openLinks, 0);
 
 })();
